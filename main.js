a6b50877bc4c85c21bc1b5e9472e61fe186971ad
PYMcn <511211322@qq.com>
2024-11-17 21:26:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginLink = document.getElementById('loginLink');
+    loginLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        alert('登录功能暂未实现！');
+    });
+});
